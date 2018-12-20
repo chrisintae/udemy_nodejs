57078950fb1971d748deed3aa823471b13565cce
chrisintae <hello@chrisintae.com>
2018-12-20 13:28:56 -0500

    Add Section 6 - Pug

---
+++ section6/pug/routes/shop.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+const adminData = require('./admin');
+
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', { pageTitle: 'shop', prods: products, path: '/' });
+});
+
+/*
+  router.get('/', (req, res, next) => {
+    console.log('shop.js', adminData.products)
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  });
+*/
+
+module.exports = router;
